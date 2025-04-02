7fc9180805971713a9806d61013485dc24813c2e
Aayush <aausgh@gmail.com>
2025-04-02 11:27:03 +0545

    eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      "semi": "error",
+      "quotes": "error",
+    }
+  }
 ];
 
 export default eslintConfig;
