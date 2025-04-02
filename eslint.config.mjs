5404d79dc1bf9ce4bec541156e4c03b295be12ff
Aayush <aausgh@gmail.com>
2025-04-02 11:39:47 +0545

    prettier sort imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
-  {
-    rules: {
-      "prefer-arrow-callback": "error",
-      "prefer-template": "error",
-      "semi": "error",
-      "quotes": "error",
-    }
-  }
+   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+   {
+      rules: {
+         "prefer-arrow-callback": "error",
+         "prefer-template": "error",
+         "semi": "error",
+         "quotes": "error",
+      }
+   }
 ];
 
 export default eslintConfig;
